448c9836af4799884c318b3ba6cc2a4cddac8c5a
lucidrains <lucidrains@gmail.com>
2023-07-27 10:47:05 -0700

    scaffold

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'VN-transformer',
+  packages = find_packages(exclude=[]),
+  version = '0.0.1',
+  license='MIT',
+  description = 'Vector Neuron Transformer (VN-Transformer)',
+  author = 'Phil Wang',
+  author_email = 'lucidrains@gmail.com',
+  long_description_content_type = 'text/markdown',
+  url = 'https://github.com/lucidrains/VN-transformer',
+  keywords = [
+    'artificial intelligence',
+    'deep learning',
+    'equivariance',
+    'vector neurons',
+    'transformers',
+    'attention mechanism'
+  ],
+  install_requires=[
+    'einops>=0.6.0',
+    'torch>=1.6'
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
