0310fc6d377bd390a6c26d420ba7c26cbafc0d6e
lucidrains <lucidrains@gmail.com>
2023-07-27 17:26:51 -0700

    start with basic vn-linear module

--- VN_transformer/VN_transformer.py
+++ VN_transformer/VN_transformer.py
@@ -3,11 +3,26 @@ import torch.nn.functional as F
 from torch import nn, einsum, Tensor
 
 from einops import rearrange, repeat, reduce
+from einops.layers.torch import Rearrange
 
 # helper
 
 def exists(val):
     return val is not None
+
+# equivariant modules
+
+class VNLinear(nn.Module):
+    def __init__(
+        self,
+        dim_in,
+        dim_out
+    ):
+        super().__init__()
+        self.weight = nn.Parameter(torch.randn(dim_out, dim_in))
+
+    def forward(self, x):
+        return einsum('... i c, o i -> ... o c', x, self.weight)
 
 # main class
 
@@ -15,9 +30,23 @@ class VNTransformer(nn.Module):
     def __init__(
         self,
         *,
-        dim
+        dim,
+        reduce_dim_out = True
     ):
         super().__init__()
+
+        self.vn_proj_in = nn.Sequential(
+            Rearrange('... c -> ... 1 c'),
+            VNLinear(1, dim)
+        )
+
+        if reduce_dim_out:
+            self.vn_proj_out = nn.Sequential(
+                VNLinear(dim, 1),
+                Rearrange('... 1 c -> ... c')
+            )
+        else:
+            self.vn_proj_out = nn.Identity()
 
     def forward(
         self,
@@ -25,4 +54,7 @@ class VNTransformer(nn.Module):
         coors,
         mask = None
     ):
+        coors = self.vn_proj_in(coors)
+
+        coors = self.vn_proj_out(coors)
         return feats, coors
